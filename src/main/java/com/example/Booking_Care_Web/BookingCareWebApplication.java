d81596042ca0c5c86daff0f6b0d439845f3b45da
nguyenanhthu03 <nguyenanhthu15082003@gmail.com>
2024-11-24 01:25:24 +0700

    cmit

--- src/main/java/com/example/Booking_Care_Web/BookingCareWebApplication.java
+++ src/main/java/com/example/Booking_Care_Web/BookingCareWebApplication.java
@@ -5,8 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BookingCareWebApplication {
+	public static final String GOOGLE_ID = System.getenv("FACEBOOK_APP_ID");
+	public static final String  GOOGLE_SECRET = System.getenv("FACEBOOK_CLIENT_SECRET");
 
 	public static void main(String[] args) {
+		System.out.println("Facebook Client ID: " + GOOGLE_ID);
+		System.out.println("Facebook Client Secret: " + GOOGLE_SECRET);
 		SpringApplication.run(BookingCareWebApplication.class, args);
 	}
 

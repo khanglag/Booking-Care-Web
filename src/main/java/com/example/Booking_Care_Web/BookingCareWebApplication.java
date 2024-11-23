ef481abddb164ab644db2c8650537861843a3529
Minh Khang <94234570+khanglag@users.noreply.github.com>
2024-11-23 18:01:04 +0700

    u

--- src/main/java/com/example/Booking_Care_Web/BookingCareWebApplication.java
+++ src/main/java/com/example/Booking_Care_Web/BookingCareWebApplication.java
@@ -7,9 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BookingCareWebApplication {
 
 	public static void main(String[] args) {
-		System.out.println("GOOGLE_CLIENT_ID: " + System.getenv("GOOGLE_CLIENT_ID"));
-		System.out.println("GOOGLE_CLIENT_SECRET: " + System.getenv("GOOGLE_CLIENT_SECRET"));
-
 		SpringApplication.run(BookingCareWebApplication.class, args);
 	}
 

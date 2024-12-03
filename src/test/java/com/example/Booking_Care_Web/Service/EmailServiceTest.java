063b3f98053b6408a596a764ec4762526c5e9d5b
Minh Khang <94234570+khanglag@users.noreply.github.com>
2024-12-03 17:05:59 +0700

    fix lỗi email

--- src/test/java/com/example/Booking_Care_Web/Service/EmailServiceTest.java
+++ src/test/java/com/example/Booking_Care_Web/Service/EmailServiceTest.java
@@ -1,4 +1,13 @@
 package com.example.Booking_Care_Web.Service;
 
+import com.example.Booking_Care_Web.Services.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
+
+@SpringBootTest
+@SpringJUnitWebConfig
 public class EmailServiceTest {
+    @Autowired
+    EmailService emailService;
 }
